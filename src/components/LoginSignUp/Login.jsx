3b02f11f9330fd886260dfe281ceeb8d3b9f134f
Govind Rajewar <abhirajewar87@gmail.com>
2024-08-26 10:45:25 +0530

    Login: added local storage for login

--- src/components/LoginSignUp/Login.jsx
+++ src/components/LoginSignUp/Login.jsx
@@ -1,17 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    const existingUsers = JSON.parse(localStorage.getItem("users")) || [];
+    const user = existingUsers.find(
+      (user) => user.email === email && user.password === password
+    );
+
+    if (user) {
+      alert("Login Successful!");
+      navigate("/dashboard");
+    } else {
+      alert("Invalid email or password");
+    }
+  };
+
   return (
     <div className="logIn">
       <div className="inputField">
-        <label htmlFor="">Email</label>
-        <input type="email" />
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </div>
       <div className="inputField">
-        <label htmlFor="">Password</label>
-        <input type="password" />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </div>
-      <div className="submitBtn">Log in</div>
+      <div className="submitBtn" onClick={handleLogin}>
+        Log in
+      </div>
     </div>
   );
 }
