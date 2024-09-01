5c3938dbbdc369dced40d9a6ad330a896edb8d91
Govind Rajewar <abhirajewar87@gmail.com>
2024-09-01 12:24:01 +0530

    CreateQuiz: generateQuizId added

--- src/components/Dashboard/CreateQuiz.jsx
+++ src/components/Dashboard/CreateQuiz.jsx
@@ -85,7 +85,18 @@ function CreateQuiz({
         year: "numeric",
       }).format(currentDate);
 
+      const generateQuizId = (length) => {
+        const chars =
+          "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        let result = "";
+        for (let i = 0; i < length; i++) {
+          result += chars.charAt(Math.floor(Math.random() * chars.length));
+        }
+        return result;
+      };
+
       const newQuizData = {
+        quizId: generateQuizId(8),
         quizName,
         quizType,
         questions: questionsData,
