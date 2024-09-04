e6c097ad606e5a8c719e69af3040a5f6a583baf0
Govind Rajewar <abhirajewar87@gmail.com>
2024-09-04 13:17:07 +0530

    Updated Server Link

--- src/api/User.js
+++ src/api/User.js
@@ -3,7 +3,6 @@ import axios from "axios";
 import BACKEND_ORIGIN_URL from "../links"
 
 const SignUpUser = async (name, email, password, action) => {
-  console.log("BACKEND_ORIGIN_URL"+BACKEND_ORIGIN_URL);
   try {
     const response = await axios.post(`${BACKEND_ORIGIN_URL}/`, {
       name,
--- src/links.js
+++ src/links.js
@@ -1,4 +1,4 @@
-const BACKEND_ORIGIN_URL = "https://quizzie-server-seven.vercel.app";
+const BACKEND_ORIGIN_URL = "https://quizzie-server-0461.onrender.com/";
 const FRONTEND_ORIGIN_URL = "https://quizzie-blush.vercel.app";
 
 // eslint-disable-next-line
